bb7e5e5dd4b2f238c9b1048c6b879fef08f6e61a
jesusRodvid <60220649+jesusRodvid@users.noreply.github.com>
2021-02-20 12:51:22 +0000

    Bugg en las notas arreglado
    las notas no se descolocan al pulsar ALT

--- src/main/java/dad/productividad/note/NotesController.java
+++ src/main/java/dad/productividad/note/NotesController.java
@@ -14,6 +14,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.FlowPane;
@@ -55,7 +56,9 @@ public class NotesController implements Initializable{
 
 		scrollpane.setFitToWidth(true);
 		readNotes();
-
+		//notesWrapper.centerShapeProperty();
+		notesWrapper.clearLayout();
+		notesWrapper.setPadding(new Insets(0, 30, 30, 140));
 	}
 
 	private void readNotes() {
