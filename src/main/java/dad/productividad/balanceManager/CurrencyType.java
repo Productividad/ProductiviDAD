4572b746a3817621508593dbdcfa7d7015ecd4c4
imgenki <s.rguez00@gmail.com>
2021-02-23 19:30:04 +0000

    i don't know what I did, but I did it

--- src/main/java/dad/productividad/balanceManager/CurrencyType.java
+++ src/main/java/dad/productividad/balanceManager/CurrencyType.java
@@ -1,17 +1,27 @@
 package dad.productividad.balanceManager;
 
+import java.util.ResourceBundle;
+
 public enum CurrencyType {
 
-    EURO("€"), POUND("£"), DOLAR("$"), YEN("¥"), SWISS_FRANC("₣");
-    private final String text;
+    EURO("€", "Euro"), POUND("£", ResourceBundle.getBundle("i18n/strings").getString("pound")),
+    DOLLAR("$", ResourceBundle.getBundle("i18n/strings").getString("dollar")), YEN("¥", "Yen"),
+    SWISS_FRANC("₣", ResourceBundle.getBundle("i18n/strings").getString("franc")), YUAN("¥", "Yuan");
 
-    CurrencyType(final String text) {
-        this.text = text;
+    private final String symbol;
+    private final String nm;
 
+    CurrencyType(String symbol, String name) {
+        this.symbol = symbol;
+        this.nm = name;
     }
 
-	@Override
+    @Override
 	public String toString() {
-		return text;
+		return symbol + " " + nm;
 	}
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
