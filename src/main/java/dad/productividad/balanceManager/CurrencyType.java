06618fb6d042b0759c668595e4d554c45b530171
imgenki <s.rguez00@gmail.com>
2021-02-23 18:41:00 +0000

    Final update?

--- src/main/java/dad/productividad/balanceManager/CurrencyType.java
+++ src/main/java/dad/productividad/balanceManager/CurrencyType.java
@@ -2,5 +2,16 @@ package dad.productividad.balanceManager;
 
 public enum CurrencyType {
 
-	EURO,POUND,DOLAR,YEN,SWISS_FRANC,YUAN;	
+    EURO("€"), POUND("£"), DOLAR("$"), YEN("¥"), SWISS_FRANC("₣"), YUAN("¥");
+    private final String text;
+
+    CurrencyType(final String text) {
+        this.text = text;
+
+    }
+
+	@Override
+	public String toString() {
+		return text;
+	}
 }
