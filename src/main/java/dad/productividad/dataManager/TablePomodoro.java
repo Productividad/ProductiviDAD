92c24755c09a794ef6d93ef7ad010314f2b5f4d3
imgenki <s.rguez00@gmail.com>
2021-02-28 17:56:16 +0000

    Update TablePomodoro.java

--- src/main/java/dad/productividad/dataManager/TablePomodoro.java
+++ src/main/java/dad/productividad/dataManager/TablePomodoro.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import dad.productividad.app.MainController;
 import dad.productividad.note.Note;
 import dad.productividad.pomodoro.PomodoroSetup;
 
@@ -12,12 +13,13 @@ import dad.productividad.pomodoro.PomodoroSetup;
  * Class used to interact with the Pomodoro table in the database.
  */
 public class TablePomodoro {
-	 /**
-	  * Inserts new pomodoro into the database
-	  * @param pomodoroSetup The pomodoro to be inserted
-	  */
+    /**
+     * Inserts new pomodoro into the database
+     *
+     * @param pomodoroSetup The pomodoro to be inserted
+     */
     public static void insertPomodoro(PomodoroSetup pomodoroSetup) {
-        String insert = "INSERT INTO pomodoro (title_pomodoro, time_spent,FK_ID_page,FK_ID_task) VALUES ( ?, ?, ?)";
+        String insert = "INSERT INTO pomodoro (title_pomodoro, time_spent, FK_ID_page) VALUES (?, ?, (SELECT id_page FROM pages where date_page=date('now'))";
         String getPkId = "SELECT seq FROM sqlite_sequence WHERE name='pomodoro'";
         int id = 0;
         try {
@@ -25,9 +27,6 @@ public class TablePomodoro {
             PreparedStatement pstmt = JdbcConnection.connection.prepareStatement(insert);
             pstmt.setString(1, pomodoroSetup.getTitlePomodoro());
             pstmt.setInt(2, pomodoroSetup.getTimeSpent());
-            pstmt.setInt(2, pomodoroSetup.getTimeSpent());
-            pstmt.setInt(2, pomodoroSetup.getIdPage());
-            pstmt.setInt(2, pomodoroSetup.getIdTask());
 
             pstmt.executeUpdate();
 
@@ -38,15 +37,15 @@ public class TablePomodoro {
                 id = rs.getInt("seq");
             }
             pomodoroSetup.setIdPomodoro(id);
+            pomodoroSetup.setIdPage(MainController.getTodaysPage().getId());
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             JdbcConnection.close();
         }
 
- 
+
     }
-	
-	
-	
+
+
 }
